da7150b010bcb2590913e3c38c1a8d909fd28093
Brandon Love <branlove17@yahoo.com>
2016-12-11 16:51:02 -0500

    HelloWorld.java
    Intinital commit for HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//  HelloWorld tutorial for github
+
+//  @author: branlove17
+
+
+public class HelloWorld 
+{    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World");
+    }
+}
